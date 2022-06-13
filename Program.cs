d4c97d802a264beabedd719c0aaee8367ca18dc7
Cami <camilacassimiro@gmail.com>
2022-06-13 11:27:51 -0300

    enviei exercícios

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@
       @"Seja bem vindo(a) ao GeraEstoque 1.0
 ------------------------------------";
       System.Console.WriteLine(titulo);
-
       mostrarOpcoes();
+      var opcao = short.Parse(Console.ReadLine());
 
+      switch (opcao)
+      {
+        case 1: CadastrarProduto(); break;
+        default: mostrarOpcoes(); break;
+      };
     }
 
     public static void mostrarOpcoes()
@@ -25,27 +30,55 @@
 
       System.Console.WriteLine(opcoes);
       System.Console.WriteLine("Digite uma opção");
-      CadastrarProduto();
-
-      Console.ReadLine();
-
     }
     public static void CadastrarProduto()
     {
-      // System.Console.WriteLine("Digite uma opção");
-      var opcao = Console.ReadLine();
+      string id;
+      string descricao;
+      int quantidade;
+      double valorCompra;
+      double valorVenda;
+      char continua = 'S';
+      Guid guid = Guid.NewGuid();
 
-      // Console.WriteLine("Digite uma opção");
-
-
-      switch (opcao)
+      do
       {
-        case 1: Program.CadastrarProduto(); break;
-        default: mostrarOpcoes(); break;
-      }
 
-      Console.ReadLine(opcao);
-      mostrarOpcoes();
+        Console.WriteLine("Cadastramento");
+
+        Console.WriteLine("Insira a descrição do produto:");
+        descricao = Console.ReadLine();
+
+        Console.WriteLine("Insira a quantidade:");
+        quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("insira o valor de compra:");
+        valorCompra = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("insira o valor de venda:");
+        valorVenda = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Deseja continuar? (S/N)");
+        continua = Convert.ToChar(Console.ReadLine());
+
+        id = guid.ToString();
+
+      } while (continua == 'S');
+
+      Console.Clear();
+
+      Console.WriteLine("Produto cadastrado com sucesso!");
+
+      Console.WriteLine($"Nome: {descricao}");
+      Console.WriteLine($"Quantidade: {quantidade}");
+      Console.WriteLine($"{valorCompra}");
+      Console.WriteLine($"{valorVenda}");
+      Console.WriteLine($"id: {id}");
+
+    }
+    static void criaGuid()
+    {
+      Guid guid = Guid.NewGuid();
     }
   }
 }
