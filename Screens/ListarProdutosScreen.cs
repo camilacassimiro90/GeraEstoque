59ffb8bc587996de5c4d03d21f9c182891529e6e
Camila <camilacassimiro@gmail.com>
2022-06-18 15:12:34 -0300

    add ex 09

--- Screens/ListarProdutosScreen.cs
+++ Screens/ListarProdutosScreen.cs
@@ -1,8 +1,19 @@
-namespace GeraEstoque.Screens
+using GeraEstoque.Models;
+using GeraEstoque.Repositories;
+namespace GeraEstoque.Screens;
+
+
+class ListarProdutosScreen
 {
-  public static class ListarProdutoScreen
+  public static void Iniciar(ProdutoRepository repository)
   {
-
-
+    Console.Clear();
+    System.Console.WriteLine("Lista de Produtos Cadastrados");
+    System.Console.WriteLine("--------------------------------");
+    foreach (var produto in repository.Produtos)
+    {
+      System.Console.WriteLine(produto.ToString());
+    }
+    Console.ReadLine();
   }
 }
